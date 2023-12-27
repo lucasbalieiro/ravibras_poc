2028d8aa0011f94950fd74a74c0d193769b24039
Lucas Balieiro <lucasbalieirom@hotmail.com>
2023-12-27 15:23:18 -0400

    set facingMode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,21 @@ import Webcam from 'react-webcam'
 import jig from './jig.png'
 import Image from 'next/image'
 export default function Home() {
+
+  const videoConstraints = {
+    facingMode: { exact: "environment" }
+  };
   return (
     <>
       <h1>POC Ravibras</h1>
       <Image src={jig} alt='jig' style={{
         position: 'absolute',
         top: 10,
-        left:100
+        left: 50
       }} />
-      <Webcam />
+      <Webcam
+        videoConstraints={videoConstraints}
+      />
 
     </>
   )
